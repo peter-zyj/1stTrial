502ce996ec41074503e78238772d6f59fdcfb7ba
peter-zyj <peter_zyj@hotmail.com>
2013-12-09 21:39:59 +0800

    home2

--- linphone-android/submodules/externals/build/ffmpeg/Android.mk
+++
@@ -1,3 +0,0 @@
-include  $(linphone-root-dir)/submodules/externals/build/ffmpeg/Android_libavutil.mk
-include  $(linphone-root-dir)/submodules/externals/build/ffmpeg/Android_libavcodec.mk
-include  $(linphone-root-dir)/submodules/externals/build/ffmpeg/Android_libswscale.mk
--- linphone-android/submodules/externals/build/libvpx/Android.mk
+++
@@ -1,7 +0,0 @@
-LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := libvpx
-LOCAL_SRC_FILES := $(TARGET_ARCH)/libvpx.a
-
-include $(PREBUILT_STATIC_LIBRARY)
